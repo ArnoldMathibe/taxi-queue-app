49aba13d06e672e5c0626e89eb576b4b8ca41e36
Arnold Kgaladi <arnoldtiisetso3@gmail.com>
2022-05-30 14:17:19 +0200

    Simnandi

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,22 @@
 
 
 // DOM element references
+const queueCountElem = document.querySelector('.passenger_queue_count');
+const join_Queue = document.querySelector('.join_queue');
+const leave_queue = document.querySelector('.leave_queue');
+
+const taxi_queue_countElem = document.querySelector('.taxi_queue_count');
+const join_taxi_queue = document.querySelector('.join_taxi_queue');
+
+const depart = document.querySelector('.depart')
 
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
 
-// DOM events
+const joinPeopleQueue = TaxiQueue();
 
+// DOM events
+join_Queue.addEventListener('click', function(){
+    joinPeopleQueue.joinQueue();
+    queueCountElem.innerHTML = joinPeopleQueue.queueLength();
+})
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,40 @@
-function TaxiQueue() {
-
+function TaxiQueue(_peopleCount, _taxi) {
+	let peopleCount = 0;
+	let taxiQueue = 0;
+/*
+	if (_peopleCount) {
+		peopleCount = _peopleCount;
+	}
+	if (_taxi) {
+		taxi = _taxi;
+	}*/
 
 	function joinQueue() {
-
+		return peopleCount++;
 	}
 
 	function leaveQueue() {
-
+		if (peopleCount > 0) {
+			return peopleCount--;
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		return taxiQueue++;
 	}
 
 	function queueLength() {
-
+		return peopleCount;
 	}
 
 	function taxiQueueLength() {
-
+		return taxiQueue;
 	}
 
 	function taxiDepart(){
-
+		if (peopleCount >= 12) {
+			return taxiQueue =- taxiQueueLength(peopleCount - 12);
+		}
 	}
 
 	return {
